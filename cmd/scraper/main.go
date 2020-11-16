61cee3f2dcb7095e341a8e05fbd3a53ffe6d0c28
Sergi Orra <sergiorra17@gmail.com>
2020-11-16 16:08:40 +0100

    refactor

--- cmd/scraper/main.go
+++ cmd/scraper/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"github.com/sergiorra/scraping-prices-go/booking"
-	"github.com/sergiorra/scraping-prices-go/pricetravel"
-	"github.com/sergiorra/scraping-prices-go/trip"
+	"github.com/sergiorra/scraping-prices-go/internal/booking"
+	"github.com/sergiorra/scraping-prices-go/internal/pricetravel"
+	"github.com/sergiorra/scraping-prices-go/internal/trip"
 )
 
 func main() {
